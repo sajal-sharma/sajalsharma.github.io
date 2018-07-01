a6201dd7f38fe369f01389b73befe78369166d00
Sajal Sharma <sajal16.sharma@gmail.com>
2018-06-30 20:11:29 -0700

    Moved navigation bar to the left

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
             var hash = this.hash;
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
-            }, 1250, function(){
+            }, 1000, function(){
                 window.location.hash = hash;
             });
         }
